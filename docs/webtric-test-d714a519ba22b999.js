e5ff91569096bbb13e202ce9422ae0c8153e84e1
Acheul <seoul605.21@gmail.com>
2024-04-23 05:57:36 +0900

    init

--- docs/webtric-test-d714a519ba22b999.js
+++ docs/webtric-test-d714a519ba22b999.js
@@ -788,7 +788,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('webtric-test-7eaec0e7057392a8_bg.wasm', import.meta.url);
+        input = new URL('webtric-test-d714a519ba22b999_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
